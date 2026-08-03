a6694981366791c360e50ad90282608f4d0f4164
Lovepreet Singh <lovepreetsingh200626@gmail.com>
2026-08-04 01:21:08 +0530

    icon updated

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -348,7 +348,7 @@ export default function AuthModal({ onLoginSuccess }) {
             <div>
               <label className="text-xs font-bold text-slate-500 uppercase tracking-wider">6-Digit OTP</label>
               <div className="relative mt-1 group">
-                <Hash className="absolute left-3.5 top-3.5 w-4 h-4 text-slate-400 group-focus-within:text-blue-600 transition" />
+                <User className="absolute left-3.5 top-3.5 w-4 h-4 text-slate-400 group-focus-within:text-blue-600 transition" />
                 <input required type="text" maxLength="6" placeholder="Enter OTP received on email" value={resetData.otp} onChange={e => setResetData({ ...resetData, otp: e.target.value.replace(/\D/g, '') })} className="w-full bg-slate-50 border border-slate-300 rounded-xl py-3 pl-11 pr-4 text-slate-900 text-sm focus:outline-none focus:border-blue-600 tracking-widest font-bold" />
               </div>
             </div>
@@ -449,7 +449,7 @@ export default function AuthModal({ onLoginSuccess }) {
                   <div>
                     <label className="text-xs font-bold text-slate-500 uppercase tracking-wider">Roll Number</label>
                     <div className="relative mt-1 group">
-                      <Hash className="absolute left-3.5 top-3.5 w-4 h-4 text-slate-400 group-focus-within:text-blue-600 transition" />
+                      <User className="absolute left-3.5 top-3.5 w-4 h-4 text-slate-400 group-focus-within:text-blue-600 transition" />
                       <input required type="text" maxLength="3" placeholder="Enter your Roll Number" value={formData.rollNo} onChange={e => setFormData({ ...formData, rollNo: e.target.value })} className="w-full bg-slate-50 border border-slate-300 rounded-xl py-3 pl-11 pr-4 text-slate-900 text-sm focus:outline-none focus:border-blue-600 uppercase" />
                     </div>
                   </div>
